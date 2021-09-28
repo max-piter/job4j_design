6a2677b3db87d165f996368f0810a79d09871adb
max-piter <61771015+max-piter@users.noreply.github.com>
2021-09-28 21:42:43 +0300

    1. Что такое итератор. [#4951]

---
+++ src/main/java/ru/job4j/it/BackwardArrayIt.java
@@ -0,0 +1,41 @@
+package ru.job4j.it;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+/**
+ * класс BackwardArrayIt имплементирует Iterator<Integer>
+ *
+ * внутри реализован итератор для массива.
+ * Итератор должен отдавать элементы в обратном порядке.
+ * в конструкторе присвоил  значение int переменной point  -  конец массива,
+ * в методе next() шагаю от неё,
+ * пока не сравняюсь с 0 (что  прописано в методе hasNext(), который контолирует диапазон итерации)
+ *
+ */
+
+
+public class BackwardArrayIt implements Iterator<Integer> {
+
+    private final int[] data;
+    private int point;
+
+
+    public BackwardArrayIt(int[] data) {
+        this.data = data;
+        this.point = data.length - 1;
+    }
+
+    @Override
+    public boolean hasNext() {
+        return point >= 0;
+    }
+
+    @Override
+    public Integer next() {
+        if (!hasNext()) {
+            throw new NoSuchElementException();
+        }
+        return data[point--];
+    }
+}
