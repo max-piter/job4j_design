afa9d8d696007298b39ee6321fe0d5b678371989
max-piter <61771015+max-piter@users.noreply.github.com>
2021-10-20 08:50:13 +0300

    7. ListIterator [#350217]

--- src/main/java/ru/job4j/iterator/ListUtils.java
+++ src/main/java/ru/job4j/iterator/ListUtils.java
@@ -5,8 +5,19 @@ import java.util.ListIterator;
 import java.util.Objects;
 import java.util.function.Predicate;
 
+/**
+ * класс ListUtils -  является хранилищем методов ListIterator
+ */
 public class ListUtils {
 
+    /**
+     * Add before. метод  добавляет объект перед указанным индексом
+     *
+     * @param <T>   the type parameter
+     * @param list  коллекция
+     * @param index индекс, перед  которым нужно разместить объект
+     * @param value объект, который необходимо поместить в коллекцию
+     */
     public static <T> void addBefore(List<T> list, int index, T value) {
         Objects.checkIndex(index, list.size());
         ListIterator<T> i = list.listIterator();
@@ -19,6 +30,14 @@ public class ListUtils {
         }
     }
 
+    /**
+     * Add after -  метод добавляет объект в коллекцию  после заданного индекса
+     *
+     * @param <T>   the type parameter
+     * @param list  коллекция
+     * @param index индекс, после   которого нужно разместить объект
+     * @param value объект, который необходимо поместить в коллекцию
+     */
     public static <T> void addAfter(List<T> list, int index, T value) {
         Objects.checkIndex(index, list.size());
         ListIterator<T> l = list.listIterator();
@@ -35,6 +54,12 @@ public class ListUtils {
         }
     }
 
+    /**
+     * Remove if метод удаляет элементы  удовлетворяющие условию, указанному в предикате
+     * @param <T>    the type parameter
+     * @param list   коллекция элементов
+     * @param filter предикат,  по которому определяют удаляемый объект
+     */
     public static <T> void removeIf(List<T> list, Predicate<T> filter) {
        ListIterator<T> l = list.listIterator();
        while (l.hasNext()) {
@@ -46,6 +71,14 @@ public class ListUtils {
 
     }
 
+    /**
+     * Replace if метод заменяет объект удолетворяющий предикату на другой
+     *
+     * @param <T>    the type parameter
+     * @param list   коллеция объектов типа Т
+     * @param filter предикат
+     * @param value  объект, который необходимо поместить на место удалённого
+     */
     public static <T> void replaceIf(List<T> list, Predicate<T> filter, T value) {
         ListIterator<T> l = list.listIterator();
         while (l.hasNext()) {
@@ -56,6 +89,13 @@ public class ListUtils {
         }
     }
 
+    /**
+     * Remove all метод удаляет из  коллекции все элементы, которые помещены в другую коллекцию
+     *
+     * @param <T>      the type parameter
+     * @param list     коллеция из  которой необходимо удалить элементы
+     * @param elements коллекция элементов,  которые необходимо удалить.
+     */
     public static <T> void removeAll(List<T> list, List<T> elements) {
         ListIterator<T> l = elements.listIterator();
         while (l.hasNext()) {
