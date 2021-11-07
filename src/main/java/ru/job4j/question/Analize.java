6092488409ba3e252dd8a6d07fc10b0577c97074
max-piter <61771015+max-piter@users.noreply.github.com>
2021-11-07 18:16:55 +0300

    2. Статистика по коллекции. [#45889 #224908]

--- src/main/java/ru/job4j/question/Analize.java
+++ src/main/java/ru/job4j/question/Analize.java
@@ -1,8 +1,8 @@
 package ru.job4j.question;
 
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
-import java.util.TreeMap;
 
 /**
  * The type Analize. класс содержит единственный метод, для анализа списков User
@@ -11,39 +11,40 @@ public class Analize {
 
     /**
      * метод производит аналитику изменений в списках
-     * спользуем структуру TreeMap, чтобы  поиск по значению занимал O(logN) времени
+     * спользуем структуру HashMap
+     * внутри используем метод putIfAbsent(), помимо добавления отсутствующего элемента,
+     * он возвращает все value, через  которые проходит - этим и воспользуемся
+     *
      * @param previous - первоначальный список
-     * @param current -  конечный список
+     * @param current  -  конечный список
      * @return - возвращает объект класса Info с полученными данными
      */
     public static Info diff(Set<User> previous, Set<User> current) {
-        Info info = new Info(0, 0, 0);
+        int countChanged = 0;
+        int countAdded = 0;
 
-        Map<Integer, String> usersPrevious = new TreeMap<>();
-        Map<Integer, String> usersCurrent = new TreeMap<>();
+        Map<Integer, String> usersPrevious = new HashMap<>();
 
-        for (User us: previous) {
+        for (User us : previous) {
             usersPrevious.put(us.getId(), us.getName());
         }
 
-        for (User us: current) {
-            usersCurrent.put(us.getId(), us.getName());
-        }
+        for (User us : current) {
+            String previousUserName = usersPrevious.putIfAbsent(us.getId(), us.getName());
+            if (previousUserName != null) {
+                if (!us.getName().equals(previousUserName)) {
+                    countChanged++;
+                }
+            } else {
+                    countAdded++;
+                }
+            }
 
-        info.setChanged((int) usersPrevious.entrySet().stream()
-                .filter(us -> usersCurrent.containsKey(us.getKey()))
-                .filter(us -> !us.getValue().equals(usersCurrent.get(us.getKey())))
-                .count());
-
-        info.setAdded((int) usersCurrent.entrySet().stream()
-                .filter(us -> !usersPrevious.containsKey(us.getKey()))
-                .count());
-
-        info.setDeleted((int) usersPrevious.entrySet().stream()
-                .filter(us -> !usersCurrent.containsKey(us.getKey()))
-                .count());
-
-        return info;
+        int countDeleted = previous.size() - current.size() + countAdded;
+        return new Info(countAdded, countChanged, countDeleted);
     }
-
 }
+
+
+
+
--- src/test/java/ru/job4j/question/AnalizeTest.java
+++ src/test/java/ru/job4j/question/AnalizeTest.java
@@ -1,7 +1,6 @@
 package ru.job4j.question;
 
 import org.junit.Test;
-
 import java.util.Set;
 import static org.hamcrest.core.Is.is;
 import static org.hamcrest.MatcherAssert.assertThat;
