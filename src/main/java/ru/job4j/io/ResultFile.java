cb3610bc770062bb2728265655517bee03c52520
max-piter <61771015+max-piter@users.noreply.github.com>
2021-11-07 23:00:38 +0300

    0.1. FileOutputStream. [#252488]

---
+++ src/main/java/ru/job4j/io/ResultFile.java
@@ -0,0 +1,28 @@
+package ru.job4j.io;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class ResultFile {
+    public static void main(String[] args) {
+        int size = 9;
+        int[][] array =  new int[size][size];
+        try {
+            FileOutputStream fileOut = new FileOutputStream("Matrix.text");
+            for (int i = 0; i < size; i++) {
+                for (int j = 0; j < size; j++) {
+                    array[i][j] =   ((i + 1) * (j + 1));
+                    String s = array[i][j] + "   ";
+                    if (array[i][j] > 10) {
+                        s = array[i][j] + "  ";
+                    }
+            fileOut.write(s.getBytes());
+                }
+            fileOut.write(System.lineSeparator().getBytes());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
