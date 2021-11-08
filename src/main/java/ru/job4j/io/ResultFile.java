577708595468507f979edf7d7bf283eb59f3b2d1
max-piter <61771015+max-piter@users.noreply.github.com>
2021-11-08 18:36:44 +0300

    0.1. FileOutputStream. [#252488 #225994]

--- src/main/java/ru/job4j/io/ResultFile.java
+++ src/main/java/ru/job4j/io/ResultFile.java
@@ -7,8 +7,7 @@ public class ResultFile {
     public static void main(String[] args) {
         int size = 9;
         int[][] array =  new int[size][size];
-        try {
-            FileOutputStream fileOut = new FileOutputStream("Matrix.text");
+        try (FileOutputStream fileOut = new FileOutputStream("Matrix.text")) {
             for (int i = 0; i < size; i++) {
                 for (int j = 0; j < size; j++) {
                     array[i][j] =   ((i + 1) * (j + 1));
