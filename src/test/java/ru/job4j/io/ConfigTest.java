654270fdabbbcd68e81bf5db09fb7f2899fc20e0
max-piter <61771015+max-piter@users.noreply.github.com>
2021-11-10 17:29:13 +0300

    01. Читаем файл конфигурации [#858 #226616]

--- src/test/java/ru/job4j/io/ConfigTest.java
+++ src/test/java/ru/job4j/io/ConfigTest.java
@@ -10,7 +10,7 @@ public class ConfigTest {
 
     @Test
     public void whenPairWithoutComment() {
-        String path = "/Users/a123/projects/job4j_design/app.properties";
+        String path = "./data/app.properties";
         Config config = new Config(path);
         config.load();
         assertThat(config.value("hibernate.dialect"),
@@ -20,7 +20,7 @@ public class ConfigTest {
 
     @Test
     public void whenCommentInFile() {
-        String path = "/Users/a123/projects/job4j_design/app.properties";
+        String path = "./data/app.properties";
         Config config = new Config(path);
         config.load();
         assertThat(config.value("hibernate.connection.url"),
@@ -31,7 +31,7 @@ public class ConfigTest {
 
     @Test
     public void whenPairWithComment() {
-        String path = "/Users/a123/projects/job4j_design/pair_without_comment.properties";
+        String path = "./data/pair_without_comment.properties";
         Config config = new Config(path);
         config.load();
         assertThat(config.value("Max"), is("Korovkin"));
@@ -40,7 +40,7 @@ public class ConfigTest {
 
     @Test
     public void whenPairWithEmptyString() {
-        String path = "/Users/a123/projects/job4j_design/pair_without_comment.properties";
+        String path = "./data/pair_without_comment.properties";
         Config config = new Config(path);
         config.load();
         assertThat(config.value("Piter"), is("Cooper"));
@@ -49,7 +49,7 @@ public class ConfigTest {
 
     @Test
     public void whenCheckTheRestMap() {
-        String path = "/Users/a123/projects/job4j_design/app.properties";
+        String path = "./data/app.properties";
         Config config = new Config(path);
         config.load();
         assertThat(config.value("hibernate.connection.driver_class"),
@@ -60,7 +60,7 @@ public class ConfigTest {
 
     @Test(expected = IllegalArgumentException.class)
     public void whenPairWithNullValue() {
-        String path = "/Users/a123/projects/job4j_design/pair_with_NullValue.properties";
+        String path = "./data/pair_with_NullValue.properties";
         Config config = new Config(path);
         config.load();
         config.value("Nottingham");
