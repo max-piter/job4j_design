c5ccfeea29dd3cded0b910abcb715efc3754f0e5
max-piter <61771015+max-piter@users.noreply.github.com>
2021-11-18 08:24:58 +0300

    4.2. Поиск дубликатов [#315066]

--- src/main/java/ru/job4j/io/duplicates/DuplicatesFinder.java
+++ src/main/java/ru/job4j/io/duplicates/DuplicatesFinder.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.*;
 
 
 /**
@@ -14,8 +15,10 @@ import java.nio.file.Paths;
 public class DuplicatesFinder {
     public static void main(String[] args) throws IOException {
         DuplicatesVisitor visitor = new DuplicatesVisitor();
-        Path start = Paths.get("/Users/a123/music");
+        Path start = Paths.get("/Users/a123/music/");
         Files.walkFileTree(start, visitor);
+        List<Path> duplicates = visitor.getDuplicates();
+        duplicates.forEach(System.out::println);
     }
 }
 
--- src/main/java/ru/job4j/io/duplicates/DuplicatesVisitor.java
+++ src/main/java/ru/job4j/io/duplicates/DuplicatesVisitor.java
@@ -8,34 +8,55 @@ import java.util.*;
 /**
  * The type Duplicates visitor.
  * class - logic to search for duplicates
+ * using Map & List
  */
 public class DuplicatesVisitor extends SimpleFileVisitor<Path> {
 
     /**
      * The Files.
-     * Set conteiner to store non duplicates
+     * Map conteiner to store  duplicates
      */
-    Set<FileProperty> files =  new HashSet<>();
+    Map<FileProperty, List<Path>> files = new HashMap<>();
 
     /**
      * FileVisitResult.
      * method to be performed while visiting the current file
-     * find duplicates
+     * find duplicates, storing them in arrayList
      * @param file - current file
      * @param attrs - file attributes
-     * @return file
-     * @throws IOException
+     * @return file - fole
+     * @throws IOException - excetption
      */
     @Override
     public FileVisitResult visitFile(Path file, BasicFileAttributes attrs)
         throws IOException {
-        FileProperty fP = new FileProperty(attrs.size(), file.toFile().getName());
-        if (files.contains(fP)) {
-            System.out.println("Duplicate: " + file.toAbsolutePath());
-        } else {
-            files.add(fP);
-        }
 
-        return FileVisitResult.CONTINUE;
+        FileProperty fP = new FileProperty(attrs.size(), file.getFileName().toString());
+        if (files.containsKey(fP)) {
+            List<Path> duplicats = new ArrayList<>(files.get(fP));
+            duplicats.add(file);
+            files.put(fP, duplicats);
+
+        } else {
+            List<Path> lst = new ArrayList<>();
+            lst.add(file);
+            files.put(fP, lst);
+        }
+        return super.visitFile(file, attrs);
+    }
+
+    /**
+     * Gets duplicates.
+     * method for getting duplicates.
+     *
+     * @return the duplicates - list of duplicates
+     */
+    public List<Path> getDuplicates() {
+        List<Path> dup = new ArrayList<>();
+        files.values()
+                .stream()
+                .filter(el -> el.size() > 1)
+                .forEach(dup::addAll);
+        return dup;
     }
 }
