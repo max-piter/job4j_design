8ac7813280432c5a909bee60dbd693798d771cba
max-piter <61771015+max-piter@users.noreply.github.com>
2021-11-22 18:57:49 +0300

    5.2. Архивировать проект [#861]

--- src/main/java/ru/job4j/io/Zip.java
+++ src/main/java/ru/job4j/io/Zip.java
@@ -9,27 +9,17 @@ import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
 public class Zip {
-    public static void packFiles(List<File> sources, File target) {
-        try (ZipOutputStream oZip = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(target)))) {
-            for (File fileInput : sources) {
-                oZip.putNextEntry(new ZipEntry(fileInput.getPath()));
-                try (BufferedInputStream iZip = new BufferedInputStream(new FileInputStream(fileInput))) {
+    public static void packFiles(List<Path> sources, File target) {
+        try (ZipOutputStream oZip = new ZipOutputStream(
+                new BufferedOutputStream(
+                        new FileOutputStream(target)))) {
+            for (Path fileInput : sources) {
+                oZip.putNextEntry(new ZipEntry(fileInput.toFile().getAbsolutePath()));
+                try (BufferedInputStream iZip = new BufferedInputStream(new FileInputStream(String.valueOf(fileInput)))) {
                     oZip.write(iZip.readAllBytes());
                 }
             }
         } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-
-    public static void packSingleFile(File source, File target) {
-        try (ZipOutputStream zip = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(target)))) {
-            zip.putNextEntry(new ZipEntry(source.getPath()));
-            try (BufferedInputStream out = new BufferedInputStream(new FileInputStream(source))) {
-                zip.write(out.readAllBytes());
-            }
-        } catch (Exception e) {
             e.printStackTrace();
         }
     }
@@ -43,7 +33,6 @@ public class Zip {
         String exclude = argsName.get("e");
         File output = new File(argsName.get("o"));
 
-        List<File> filesForZip = new ArrayList<>();
         List<Path> filesWithoutExcluded = new ArrayList<>();
 
         try {
@@ -51,10 +40,7 @@ public class Zip {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        for (Path p : filesWithoutExcluded) {
-            filesForZip.add(p.toFile().getAbsoluteFile());
-        }
-        packFiles(filesForZip, output);
+        packFiles(filesWithoutExcluded, output);
 
     }
 }
