bd675629d5138ced062fb7f568d48f97ce83ec54
max-piter <61771015+max-piter@users.noreply.github.com>
2021-12-03 20:11:13 +0300

    4. Slf4j - вывод exception. [#268853]

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -1,5 +1,8 @@
 package ru.job4j.io;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -8,27 +11,33 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class EchoServer {
-    public static void main(String[] args) throws IOException {
-        try (ServerSocket server = new ServerSocket(9000)) {
-            while (!server.isClosed()) {
-                Socket socket = server.accept();
-                try (OutputStream out = socket.getOutputStream();
-                     BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
-                    out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+    private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
 
-                    String[] line = in.readLine().split("=");
-                    String[] subLine = line[1].split(" ");
-                    String question = subLine[0];
+    public static void main(String[] args) {
+        try {
+                try (ServerSocket server = new ServerSocket(9000)) {
+                    while (!server.isClosed()) {
+                        Socket socket = server.accept();
+                        try (OutputStream out = socket.getOutputStream();
+                             BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
+                            out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
 
-                    switch (question) {
-                        case ("Exit") -> server.close();
-                        case ("Hello") -> out.write("Hello \n".getBytes());
-                        default -> out.write("What \n".getBytes());
+                            String[] line = in.readLine().split("=");
+                            String[] subLine = line[1].split(" ");
+                            String question = subLine[0];
 
+                            switch (question) {
+                                case ("Exit") -> server.close();
+                                case ("Hello") -> out.write("Hello \n".getBytes());
+                                default -> out.write("What \n".getBytes());
+
+                            }
+                            out.flush();
+                        }
                     }
-                    out.flush();
                 }
-            }
+            } catch (IOException e) {
+            LOG.error("Error in connection", e);
         }
-    }
+        }
 }
--- src/main/java/ru/job4j/io/UsageLog4j.java
+++ src/main/java/ru/job4j/io/UsageLog4j.java
@@ -7,17 +7,10 @@ public class UsageLog4j {
     private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
 
     public static void main(String[] args) {
-        String name = "Max Korovkin, Spb";
-        char sex = 'M';
-        int age = 45;
-        byte building = 10;
-        short office = 173;
-        long logNumber = 1;
-        boolean isRealLog = false;
-        double rate = 6.6;
-
-        LOG.debug("User info name : {}, age : {}, sex: {}, isRealLog: {},"
-                       + " building: {}, office: {}, logNumber: {}, rate: {}",
-                 name, age, sex, isRealLog, building, office, logNumber, rate);
+        try {
+            throw new Exception("Not supported code");
+        } catch (Exception e) {
+            LOG.error("Exception in log example ", e);
+        }
     }
 }
