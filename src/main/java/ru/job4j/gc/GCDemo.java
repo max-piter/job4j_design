d24a24a0ca7d44088b5b07c9120717a730dc0068
max-piter <61771015+max-piter@users.noreply.github.com>
2022-04-15 20:31:44 +0300

    1. Демонстрация работы GC [#1589]

--- src/main/java/ru/job4j/gc/GCDemo.java
+++ src/main/java/ru/job4j/gc/GCDemo.java
@@ -1,5 +1,6 @@
 package ru.job4j.gc;
 
+
 public class GCDemo {
     private static final long KB = 1000;
     private static final long MB = KB * KB;
@@ -25,26 +26,14 @@ public class GCDemo {
     public static void main(String[] args) {
         System.out.println(System.getProperty("sun.management.compiler"));
 
-
         info();
-        for (int i = 0; i < 15000; i++) {
+
+        for (int i = 0; i < 1000; i++) {
             System.out.println(new User(i, "U" + i));
         }
 
-        /**
-         * -Xmx6m -Xms2m
-         *  создаём по очереди 10,  1000, 2000, 4000, 10000  объектов User
-         *  и высчитываем используемую память usedMemory
-         *
-         */
         info();
 
-        /**
-         *  пытаемся вызвать GC и проверяем состояние памяти после его работы
-         *
-         */
-        System.gc();
-        info();
     }
 
 }
