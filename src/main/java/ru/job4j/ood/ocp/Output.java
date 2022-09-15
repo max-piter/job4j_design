23dda09f0ff628839db835c18a53ca201a6484b2
max-piter <61771015+max-piter@users.noreply.github.com>
2022-09-15 11:01:47 +0300

    1. Поддержка форматом XML, JSON в генераторе отчетов. [#851]

--- src/main/java/ru/job4j/ood/ocp/Output.java
+++
@@ -1,5 +0,0 @@
-package ru.job4j.ood.ocp;
-
-public interface Output {
-    void print(String msg);
-}
--- src/main/java/ru/job4j/ood/ocp/SequenceGenerator.java
+++
@@ -1,7 +0,0 @@
-package ru.job4j.ood.ocp;
-
-import java.util.List;
-
-public interface SequenceGenerator<T> {
-    List<T> generate(int size);
-}
